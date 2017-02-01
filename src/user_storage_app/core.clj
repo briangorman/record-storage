8b1369839977e914a18a14b2e7b3672b4bc4bea2
Brian Gorman <bgorm92@gmail.com>
2017-01-31 21:18:58 -0800

    Adding support for reading in users from .csv. First attempt at sorting users and printing out

--- src/user_storage_app/core.clj
+++ src/user_storage_app/core.clj
@@ -1,9 +1,34 @@
 (ns user-storage-app.core
-  (:require [clojure.data.csv :as csv])
+  (:require [clojure.data.csv :as csv]
+            [clojure.string :refer [lower-case]])
   (:gen-class))
 
+(def users (atom []))
+
+(def my-keys [:last :first :gender :favorite-color :date-of-birth])
+
+;can this be def'd better...
+;Thought about breaking this apart, but - program requirements
+;Explicitly require adding only one line in a post
+(defn read-in-users
+  "Populates users collection from input CSV file"
+  [filename]
+  (with-open [input-file (clojure.java.io/reader filename)]
+    (doall
+      (let [data (csv/read-csv input-file)]
+        (map #(swap! users conj %) (map #(zipmap my-keys %) data))))))
+
+
+;; todo: last names
+(defn first-sort
+  "print users sorted by females first, then males. Each gender is further sorted
+   by ascending last names"
+  [x]
+  (conj
+    (sort-by #(str (:last %)) (filter #(= (compare "Male"  (:gender %)) 0 ) x))
+    (sort-by #(str (:last %)) (filter #(= (compare "Female"  (:gender %)) 0 ) x))))
 
 (defn -main
-  [& args]
-  (println "Hello, world")
-  )
+  [filename & args]
+  (read-in-users filename)
+  (println (first-sort @users)))
