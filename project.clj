b39ec0315d1231ed11e9b9332bf0fbb1fdb1f0d7
Brian Gorman <bgorm92@gmail.com>
2017-01-31 18:13:18 -0800

    Initial checkin of project. Only boilerplate layout

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject user-storage-app "0.0.0"
+  :description "Simple Application that imports users from CSV, and provides
+               a rest interface to make modifcations"
+  :url "http://github.com/briangorman"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler user-storage-app.handler/app}
+  :main ^:skip-aot user-storage-app.core
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
---
+++ src/user_storage_app/core.clj
@@ -0,0 +1,9 @@
+(ns user-storage-app.core
+  (:require [clojure.data.csv :as csv])
+  (:gen-class))
+
+
+(defn -main
+  [& args]
+  (println "Hello, world")
+  )
