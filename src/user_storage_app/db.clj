602e92c8355f1da525249f3df36ffd33aa1eee43
Brian Gorman <bgorm92@gmail.com>
2017-02-01 21:17:19 -0800

    Only allow one user to be added in read-single-user

--- src/user_storage_app/db.clj
+++ src/user_storage_app/db.clj
@@ -12,15 +12,14 @@
   [filename delimiter]
   (with-open [input-file (clojure.java.io/reader filename)]
     (doall
-      (let [data (csv/parse-csv input-file :delimiter delimiter)]
-        (map #(swap! users conj %) (map #(zipmap user-keys %) data))))))
+     (let [data (csv/parse-csv input-file :delimiter delimiter)]
+       (map #(swap! users conj %) (map #(zipmap user-keys %) data))))))
 
 (defn read-in-single-user
   "Populates users collection from input CSV file
    must pass in delimiter as char"
   [input delimiter]
-    (doall
-      (let [data (csv/parse-csv input :delimiter delimiter)]
-        (map #(swap! users conj %) (map #(zipmap user-keys %) data)))))
-
+  (doall
+   (let [data (csv/parse-csv input :delimiter delimiter)]
+     (swap! users conj (zipmap user-keys (first data))))))
 
