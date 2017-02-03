6b866b94fd30040a2c25f5087174acfcc58a54d0
Brian Gorman <bgorm92@gmail.com>
2017-02-02 18:35:17 -0800

    Adding tests for database logic

---
+++ test/user_storage_app/db_test.clj
@@ -0,0 +1,24 @@
+(ns user-storage-app.db-test
+  (:require [clojure.test :refer :all]
+            [user-storage-app.sorts :refer :all]
+            [user-storage-app.db :refer :all]))
+
+(defn db-fixture 
+  [f]
+  (reset! users [])
+  (f))
+
+(use-fixtures :each db-fixture)
+
+(def test-data "abc,cde,male,blue,11/11/1111\nxyz,wvu,female,red,01/01/2000")
+
+(deftest test-read-in-single-user
+  (testing "Test reading in users"
+      (read-in-single-user test-data \,) 
+      (is (= (count @users) 1))))
+
+(deftest test-read-in-multiple-users
+  (testing "Test reading in users"
+      (read-in-users test-data \,) 
+      (is (= (count @users) 2))))
+
